e1c4155f1fe2547a41209ac4c3b0d34eb722f2a7
Lysianthus <she@lysianth.us>
2019-06-01 02:41:34 +0800

    :tada:

---
+++ getTotalDiskUsed.js
@@ -0,0 +1,21 @@
+/*!
+ * This calculates the total disk used by all cPanel accounts under a WHM account.
+ * To use, simply run on the console on the "List Accounts" page while showing the complete list of accounts (filter "All").
+ * Tested on WHM v78.0.24.
+ * 
+ * @param  {int} n number of accounts
+ */
+function getTotalDiskUsed (n) {
+	
+	var totalDiskUsed = 0;
+
+	for(var i = 0; i < n; i++) {
+		var individualDisk = parseInt(document.getElementById('listaccts').getElementsByTagName('tr')[i+1].getElementsByClassName('number')[1].innerHTML);
+		totalDiskUsed += individualDisk;
+	}
+
+	var gb = totalDiskUsed / 1024;
+
+	console.log(totalDiskUsed  + " MiB or " + gb + " GiB");
+
+}
